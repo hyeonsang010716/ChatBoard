fbed836c6e1af36d42ba2d0c942d50cdccd256b2
chloebh9 <chloebh9@naver.com>
2024-07-25 16:31:55 +0900

    프롬프트 수정

--- src/model/Advanced_RAG/t_generation.py
+++ src/model/Advanced_RAG/t_generation.py
@@ -33,8 +33,9 @@ class Conversation:
         You are an assistant for question-answering tasks. 
         Use the following pieces of retrieved context to answer the question. 
         If you don't know the answer, just say that you don't know. 
-        Use three sentences maximum and keep the answer concise.
-        If an image description is provided, include the image description in your answer to the question.""".strip()
+        Use five sentences maximum and keep the answer concise.
+        If an image description is provided, include the image description in your answer to the question.
+        Briefly explain the reasoning behind your answer.""".strip()
 
         prompt = ChatPromptTemplate.from_messages(
             [
